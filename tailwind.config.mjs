b1af1ec78cf3a446eb4ba9c8aa24da01e6c134f4
jozef kubis <kubis.jozef@outlook.com>
2025-01-27 09:08:15 +0100

    Zalozenie projektu carriage-ride-web

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,10 +8,75 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          50: "#E1E8EF",
+          100: "#D4DEE7",
+          200: "#B7C7D7",
+          300: "#99B0C7",
+          400: "#7C99B6",
+          500: "#5E82A6",
+          600: "#4C6B8A",
+          700: "#3C546C",
+          800: "#2C3D4F",
+          900: "#1B2631",
+          950: "#141C24",
+        },
+        secondary: {
+          50: "#FFF3E0",
+          100: "#FFE0B2",
+          200: "#FFCC80",
+          300: "#FFB74D",
+          400: "#FFA726",
+          500: "#FF9800",
+          600: "#FB8C00",
+          700: "#F57C00",
+          800: "#EF6C00",
+          900: "#E65100",
+          950: "#DD2C00",
+        },
+        accent: {
+          50: "#FAF5F0",
+          100: "#F4ECE1",
+          200: "#E8D6BF",
+          300: "#DDC2A2",
+          400: "#D2AF84",
+          500: "#C69963",
+          600: "#B78343",
+          700: "#926835",
+          800: "#6C4D28",
+          900: "#4B351B",
+          950: "#382814",
+        },
+        tertiary: {
+          50: "#E8F5E9",
+          100: "#C8E6C9",
+          200: "#A5D6A7",
+          300: "#81C784",
+          400: "#66BB6A",
+          500: "#4CAF50",
+          600: "#43A047",
+          700: "#388E3C",
+          800: "#2E7D32",
+          900: "#1B5E20",
+          950: "#004D40",
+        },
+        quatertary: {
+          50: "#E1F5FE",
+          100: "#B3E5FC",
+          200: "#81D4FA",
+          300: "#4FC3F7",
+          400: "#29B6F6",
+          500: "#03A9F4",
+          600: "#039BE5",
+          700: "#0288D1",
+          800: "#0277BD",
+          900: "#01579B",
+          950: "#004BA0",
+        },
       },
     },
   },
   plugins: [],
-};
+}
+
+export default config
