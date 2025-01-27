123995072292bb8782c60f82fba2b91d98f80d5f
jozef kubis <kubis.jozef@outlook.com>
2025-01-27 12:09:50 +0100

    Upravene vzdialenosti v navigacii

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -2,25 +2,29 @@ import Link from "next/link"
 
 export default function Navigation() {
   return (
-    <nav>
-      <ul className="flex gap-16 items-center">
-        <li>
-          <Link href="/">Uvodan stranka</Link>
-        </li>
-        <li>
-          <Link href="/product">Nase jazdy</Link>
-        </li>
-        <li>
-          <Link href="/booking">Zajednat jazdu</Link>
-        </li>
-        <li>
-          <Link href="/contact">Kontaktujte nas</Link>
-        </li>
-        <li className="flex gap-2">
-          <Link href="/account">Prihlasit sa</Link>
-          {" / "}
-          <Link href="/account/registration">Registrovat</Link>
-        </li>
+    <nav className="w-full">
+      <ul className="flex items-center justify-between">
+        <div className="flex gap-20">
+          <li>
+            <Link href="/">Úvodná stránka</Link>
+          </li>
+          <li>
+            <Link href="/product">Naše jazdy</Link>
+          </li>
+          <li>
+            <Link href="/booking">Zajednať jazdu</Link>
+          </li>
+          <li>
+            <Link href="/contact">Kontaktujte nás</Link>
+          </li>
+        </div>
+        <div>
+          <li className="flex gap-2">
+            <Link href="/account">Prihlásiť sa</Link>
+            {" / "}
+            <Link href="/account/registration">Registrovať</Link>
+          </li>
+        </div>
       </ul>
     </nav>
   )
