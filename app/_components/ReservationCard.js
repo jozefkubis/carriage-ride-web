43c64de451068fd57665b43f13744513b4c0beb0
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-02-11 14:13:13 +0100

    Este viac nastylovana ReservationCard

--- app/_components/ReservationCard.js
+++ app/_components/ReservationCard.js
@@ -1,7 +1,10 @@
 "use client"
 
+import { format, parseISO } from "date-fns"
+
 export default function ReservationCard({ booking }) {
   const {
+    created_at,
     fullName,
     email,
     date,
@@ -14,9 +17,26 @@ export default function ReservationCard({ booking }) {
 
   const totalPrice = regularPrice - discount
 
+  const formattedDate = date
+    ? format(parseISO(date), "dd.MM.yyyy")
+    : "Neposkytnuté"
+
+  const formattedTime = time.slice(0, 5)
+
+  const formattedCreatedAt = created_at
+    ? format(parseISO(created_at), "dd.MM.yyyy")
+    : "Neposkytnuté"
+
   return (
     <div className="border border-gray-300 p-6 rounded-lg shadow-lg bg-white w-full mx-auto max-w-xl">
-      <h3 className="text-lg font-semibold text-gray-800 mb-4 flex items-center">📝 <span className="ml-2">{name}</span></h3>
+      <div className="flex justify-between">
+        <h3 className="text-lg font-semibold text-gray-800 mb-4 flex items-center">
+          📝 <span className="ml-2">{name}</span>
+        </h3>
+        <p>
+          <span className="font-bold">Vytvorené:</span> {formattedCreatedAt}
+        </p>
+      </div>
 
       <div className="grid grid-cols-2 gap-4 text-gray-700">
         <p>
@@ -26,10 +46,10 @@ export default function ReservationCard({ booking }) {
           <span className="font-bold">Email:</span> {email}
         </p>
         <p>
-          <span className="font-bold">Dátum:</span> {date}
+          <span className="font-bold">Dátum:</span> {formattedDate}
         </p>
         <p>
-          <span className="font-bold">Čas:</span> {time}
+          <span className="font-bold">Čas:</span> {formattedTime}
         </p>
         <p>
           <span className="font-bold">Telefón:</span> {phone}
@@ -39,18 +59,27 @@ export default function ReservationCard({ booking }) {
         </p>
       </div>
 
-      {notes && (
-        <div className="mt-4 p-4 bg-gray-100 rounded-md">
-          <p className="text-gray-700">
-            <span className="font-bold">🗒 Poznámky:</span> {notes}
-          </p>
-        </div>
-      )}
+      <div className="mt-6">
+        <span className="font-bold text-gray-700">🗒 Poznámka</span>
+        {notes ? (
+          <div className="mt-4 p-4 bg-gray-100 rounded-md">
+            <p className="text-gray-800">{notes}</p>
+          </div>
+        ) : (
+          <div className="mt-4 p-4 bg-gray-100 rounded-md">
+            <p className="text-gray-400 italic">Nemáte žiadnu poznámku...</p>
+          </div>
+        )}
+      </div>
+
 
       <div className="mt-6 flex justify-between items-center border-t pt-4">
         <span className="text-lg font-medium text-gray-900">💰 Cena:</span>
         <span className="text-2xl font-semibold text-green-600">
-          <span className="text-lg line-through mr-2">{discount ? regularPrice + "€" : ""}</span>{totalPrice}€
+          <span className="text-lg line-through mr-2">
+            {discount ? regularPrice + "€" : ""}
+          </span>
+          {totalPrice}€
         </span>
       </div>
     </div>
