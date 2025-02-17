0fdae1881eb3f2c0b21533922071d59a909ffa6d
jozef kubis <kubis.jozef@outlook.com>
2025-02-17 09:32:56 +0100

    Dokonceny BookingUpdateButton.js

--- app/_components/BookingUpdateButton.js
+++ app/_components/BookingUpdateButton.js
@@ -1,10 +1,16 @@
+import { useFormStatus } from "react-dom"
+import SpinnerMini from "./SpinnerMini"
+
 function BookingUpdateButton() {
+  const { pending } = useFormStatus()
+
   return (
     <button
       type="submit"
+      disabled={pending}
       className="w-full bg-primary-500 text-white font-semibold py-2 rounded-md hover:bg-primary-600 transition"
     >
-      Aktualizovať
+      {pending ? <SpinnerMini /> : "Aktualizovať"}
     </button>
   )
 }
