7fd9e5dcfb61e3a2798ab0ca81555265a48ac160
jozef kubis <kubis.jozef@outlook.com>
2025-02-26 10:08:34 +0100

    Vytvoreny komponent Contacts.js a v page contact pouzity suspense

---
+++ app/_components/Contacts.js
@@ -0,0 +1,63 @@
+import Link from "next/link"
+import { BsEnvelope } from "react-icons/bs"
+import { LuPhone } from "react-icons/lu"
+import { FiMapPin } from "react-icons/fi"
+import MapWrapper from "../_components/MapWrapper"
+
+export default function Contacts() {
+  const officeAddress = "Tajovskeho 8540, Zilina 010 01, Slovakia"
+
+  return (
+    <div className="flex justify-between gap-10 w-full px-32 sm:px-28 pb-20 pt-6 max-w-screen-xl">
+      {/* KONTAKTNÉ ÚDAJE */}
+      <div className="flex flex-1 flex-col gap-10 justify-center">
+        <ContactItem
+          icon={<BsEnvelope />}
+          title="Email"
+          description="Napíšte nám na"
+          detail="info@romantickejazdy.sk"
+          href="mailto:info@romantickejazdy.sk"
+        />
+        <ContactItem
+          icon={<LuPhone />}
+          title="Telefón"
+          description="Zavolajte nám na"
+          detail="+421 907 123 456"
+          href="tel:+421907123456"
+        />
+        <ContactItem
+          icon={<FiMapPin />}
+          title="Kancelária"
+          description={officeAddress}
+          detail="Trasa &rarr;"
+          href="#"
+        />
+      </div>
+
+      {/* Google Mapa */}
+      <div className="flex-1 rounded-lg flex items-center justify-center w-full h-[400px] shadow-2xl">
+        <MapWrapper address={officeAddress} />
+      </div>
+    </div>
+  )
+}
+
+function ContactItem({ icon, title, description, detail, href }) {
+  return (
+    <div className="flex items-start gap-4">
+      {/* Ikona */}
+      <div className="text-primary-500 text-3xl">{icon}</div>
+      {/* Textová časť */}
+      <div>
+        <p className="font-bold text-xl text-gray-800">{title}</p>
+        <p className="text-sm text-gray-600">{description}</p>
+        <Link
+          href={href}
+          className="text-primary-600 underline font-medium hover:text-primary-700 transition"
+        >
+          {detail}
+        </Link>
+      </div>
+    </div>
+  )
+}
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,16 +1,12 @@
-import Link from "next/link"
-import { BsEnvelope } from "react-icons/bs"
-import { LuPhone } from "react-icons/lu"
-import { FiMapPin } from "react-icons/fi"
-import MapWrapper from "../_components/MapWrapper"
+import { Suspense } from "react"
+import Contacts from "../_components/Contacts"
+import Spinner from "../_components/Spinner"
 
 export const metadata = {
   title: "Kontakt",
 }
 
 export default function Page() {
-  const officeAddress = "Tajovskeho 8540, Zilina 010 01, Slovakia"
-
   return (
     <div className="flex flex-col items-center bg-creamy-100 min-h-screen">
       {/* HEADER */}
@@ -23,59 +19,9 @@ export default function Page() {
         </p>
       </header>
 
-      {/* MAIN SECTION */}
-      <div className="flex justify-between gap-10 w-full px-32 sm:px-28 pb-20 pt-6 max-w-screen-xl">
-        {/* KONTAKTNÉ ÚDAJE */}
-        <div className="flex flex-1 flex-col gap-10 justify-center">
-          <ContactItem
-            icon={<BsEnvelope />}
-            title="Email"
-            description="Napíšte nám na"
-            detail="info@romantickejazdy.sk"
-            href="mailto:info@romantickejazdy.sk"
-          />
-          <ContactItem
-            icon={<LuPhone />}
-            title="Telefón"
-            description="Zavolajte nám na"
-            detail="+421 907 123 456"
-            href="tel:+421907123456"
-          />
-          <ContactItem
-            icon={<FiMapPin />}
-            title="Kancelária"
-            description={officeAddress}
-            detail="Trasa &rarr;"
-            href="#"
-          />
-        </div>
-
-        {/* Google Mapa */}
-        <div className="flex-1 rounded-lg flex items-center justify-center w-full h-[400px] shadow-2xl">
-          <MapWrapper address={officeAddress} />
-        </div>
-      </div>
-    </div>
-  )
-}
-
-/* KOMPONENT KONTAKTNÝCH ÚDAJOV */
-function ContactItem({ icon, title, description, detail, href }) {
-  return (
-    <div className="flex items-start gap-4">
-      {/* Ikona */}
-      <div className="text-primary-500 text-3xl">{icon}</div>
-      {/* Textová časť */}
-      <div>
-        <p className="font-bold text-xl text-gray-800">{title}</p>
-        <p className="text-sm text-gray-600">{description}</p>
-        <Link
-          href={href}
-          className="text-primary-600 underline font-medium hover:text-primary-700 transition"
-        >
-          {detail}
-        </Link>
-      </div>
+      <Suspense fallback={<Spinner />}>
+        <Contacts />
+      </Suspense>
     </div>
   )
 }
