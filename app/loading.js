fa74abd71a2491b92c977dbe6c9dee5c7276c509
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-03-02 21:23:26 +0100

    bg v loading zmenena na primary-50

--- app/loading.js
+++ app/loading.js
@@ -2,7 +2,7 @@ import Spinner from "@/app/_components/Spinner"
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center w-full h-screen">
+    <div className="flex items-center justify-center w-full h-screen bg-primary-50">
       <Spinner />
     </div>
   )
