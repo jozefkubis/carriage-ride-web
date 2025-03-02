27fca6fa11d7a95bc53207b7f99be93db107dd5a
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-03-02 21:29:23 +0100

    zmena bg v loading na creamy-100

--- app/account/loading.js
+++ app/account/loading.js
@@ -2,7 +2,7 @@ import Spinner from "@/app/_components/Spinner";
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center w-full h-screen bg-creamy-50">
+    <div className="flex items-center justify-center w-full h-screen bg-creamy-100">
       <Spinner />
     </div>
   )
